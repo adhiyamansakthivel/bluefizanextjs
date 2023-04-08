ed49c24736d19dfc4587e01fdfa89e75cf5dcd77
Adhiyaman  Sakthivel <adhiyamaninfotech@gmail.com>
2023-04-08 10:52:41 +0530

    commit_update

--- next.config.js
+++ next.config.js
@@ -11,11 +11,11 @@ const nextConfig = {
     domains : ['sangw.in', 'localhost', 'picsum.photos'] // <== Domain name
   },
   env:{
-    GET_POSTS: 'http://3.108.76.77/api/get-posts/api/get-posts',
-    GET_POSTS_ID: 'http://3.108.76.77/api/get-posts/api/get-post/',
-    GET_CATEGORIES:'http://3.108.76.77/api/get-posts/api/get-categories',
-    GET_CATEGORIES_ID: 'http://3.108.76.77/api/get-posts/api/get-category/',
-    GET_NAVIGATIONS: 'http://3.108.76.77/api/get-posts/api/get-navigations',
+    GET_POSTS: 'http://3.108.76.77/api/get-posts',
+    GET_POSTS_ID: 'http://3.108.76.77/api/get-post/',
+    GET_CATEGORIES:'http://3.108.76.77/api/get-categories',
+    GET_CATEGORIES_ID: 'http://3.108.76.77/api/get-category/',
+    GET_NAVIGATIONS: 'http://3.108.76.77/api/get-navigations',
     GET_IMAGES: 'http://3.108.76.77/api/get-posts/blogImages/',
 
   },
